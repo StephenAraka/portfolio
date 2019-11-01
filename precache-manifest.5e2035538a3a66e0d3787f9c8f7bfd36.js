12d8edc431c5165df621c2a35595a5c57d5b9a4e
StephenAraka <stevenaraka@gmail.com>
2019-11-01 18:07:46 +0300

    Updates

---
+++ precache-manifest.5e2035538a3a66e0d3787f9c8f7bfd36.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ea771a75b3cd0493de053ed042b6dd5",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a6117c15348340eb0e6f",
+    "url": "/portfolio/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "89e8f2435c829b0362f6",
+    "url": "/portfolio/static/css/main.359878a0.chunk.css"
+  },
+  {
+    "revision": "a6117c15348340eb0e6f",
+    "url": "/portfolio/static/js/2.448058dc.chunk.js"
+  },
+  {
+    "revision": "89e8f2435c829b0362f6",
+    "url": "/portfolio/static/js/main.fac749b5.chunk.js"
+  },
+  {
+    "revision": "76aa8cbd67b5c937ec2c",
+    "url": "/portfolio/static/js/runtime-main.e92c632a.js"
+  },
+  {
+    "revision": "7014dc06bac80be6f95085daaa81b0f2",
+    "url": "/portfolio/static/media/mandem.7014dc06.png"
+  },
+  {
+    "revision": "0b623bf1d65724fe03ed22b80db4f7eb",
+    "url": "/portfolio/static/media/react.0b623bf1.png"
+  }
+]);
