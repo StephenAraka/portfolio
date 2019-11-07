8af4a075348b2ef30b8fa9c4594d344222b13575
StephenAraka <stevenaraka@gmail.com>
2019-11-07 16:04:43 +0300

    Updates

---
+++ precache-manifest.3884ecb6634f6eef761f96101fe4a658.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "798a40f1047042654729bea8a7f5bff1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c417c0c528ae528e4419",
+    "url": "/portfolio/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "b9c27285d5731a6756a6",
+    "url": "/portfolio/static/css/main.20bede55.chunk.css"
+  },
+  {
+    "revision": "c417c0c528ae528e4419",
+    "url": "/portfolio/static/js/2.4be00793.chunk.js"
+  },
+  {
+    "revision": "b9c27285d5731a6756a6",
+    "url": "/portfolio/static/js/main.88569f52.chunk.js"
+  },
+  {
+    "revision": "76aa8cbd67b5c937ec2c",
+    "url": "/portfolio/static/js/runtime-main.e92c632a.js"
+  },
+  {
+    "revision": "bdfc53bcd60ca2d193280c5de43afdd0",
+    "url": "/portfolio/static/media/html.bdfc53bc.png"
+  },
+  {
+    "revision": "7014dc06bac80be6f95085daaa81b0f2",
+    "url": "/portfolio/static/media/mandem.7014dc06.png"
+  },
+  {
+    "revision": "0d8ed365842d251cd2bfa6e98d461d62",
+    "url": "/portfolio/static/media/node.0d8ed365.png"
+  },
+  {
+    "revision": "0b623bf1d65724fe03ed22b80db4f7eb",
+    "url": "/portfolio/static/media/react.0b623bf1.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.e0998fb9cd2f2dfd6302284aaf268721.js"
+  "/portfolio/precache-manifest.3884ecb6634f6eef761f96101fe4a658.js"
 );
 
 self.addEventListener('message', (event) => {
