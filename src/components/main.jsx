3ac7fde188a34016747972cc7dc49661ac4adb0f
StephenAraka <stevenaraka@gmail.com>
2022-05-06 13:00:14 +0200

    add services cards

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -250,7 +250,7 @@ const Main = () => {
       {/* <!--==================== QUALIFICATION ====================--> */}
       <section className="qualification section">
         <h2 className="section__title">Qualification</h2>
-        <span className="section__subtitle">My personal journey</span>
+        <span className="section__subtitle">My professional journey</span>
         <div className="qualification__container container">
           <div className="qualification__tabs">
             <div
@@ -392,6 +392,115 @@ const Main = () => {
 
       {/* <!--==================== SERVICES ====================--> */}
       <section className="services section" id="services">
+        <h2 className="section__title">Services</h2>
+        <span className="section_subtitle">What I offer</span>
+
+        <div className="services__container container grid">
+          {/* === SERVICES 1 === */}
+          <div className="services__content">
+            <div>
+              <i className="uil uil-web-grid services__icon"></i>
+              <h3 className="services__title">Ui/Ux <br /> Designer</h3>
+            </div>
+
+            <span className="button button--flex button--small button--link services__button">
+              View More
+              <i className="uil uil-arrow-right button__icon"></i>
+            </span>
+
+            <div className="services__modal">
+              <div className="services__modal-content">
+                <h4 className="services__modal-title">Ui/Ux <br /> Designer</h4>
+                <i className="uil uil-times services__modal-close"></i>
+
+                <ul className="services__modal-services grid">
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    I design the user interface
+                  </li>
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    Create ux element interactions
+                  </li>
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    I position your company brand
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          {/* === SERVICES 2 === */}
+          <div className="services__content">
+            <div>
+              <i className="uil uil-web-grid services__icon"></i>
+              <h3 className="services__title">Frontend <br /> Engineer</h3>
+            </div>
+
+            <span className="button button--flex button--small button--link services__button">
+              View More
+              <i className="uil uil-arrow-right button__icon"></i>
+            </span>
+
+            <div className="services__modal">
+              <div className="services__modal-content">
+                <h4 className="services__modal-title">Frontend <br /> Engineer</h4>
+                <i className="uil uil-times services__modal-close"></i>
+
+                <ul className="services__modal-services grid">
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    I implement user interface designs
+                  </li>
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    Web development
+                  </li>
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    Mobile development
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          {/* === SERVICES 3 === */}
+          <div className="services__content">
+            <div>
+              <i className="uil uil-web-grid services__icon"></i>
+              <h3 className="services__title">Backend <br /> Engineer</h3>
+            </div>
+
+            <span className="button button--flex button--small button--link services__button">
+              View More
+              <i className="uil uil-arrow-right button__icon"></i>
+            </span>
+
+            <div className="services__modal">
+              <div className="services__modal-content">
+                <h4 className="services__modal-title">Backend <br /> Engineer</h4>
+                <i className="uil uil-times services__modal-close"></i>
+
+                <ul className="services__modal-services grid">
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    I implement business logic
+                  </li>
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    Database design and integration
+                  </li>
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    Client integration
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
 
       </section>
 
