f688ae77a427a1df256083fc509926f56079a119
StephenAraka <stevenaraka@gmail.com>
2022-05-12 14:23:15 +0200

    show / hide scroll up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Main from './components/main';
 import Header from './components/Header';
 import './assets/css/App.css';
@@ -6,12 +6,21 @@ import Footer from './components/Footer';
 import ScrollToTop from './components/ScrollUp';
 
 function App() {
+  const [y, setY] = useState(window.scrollY);
+
+  useEffect(() => {
+    window.addEventListener("scroll", (e) => setY(window.scrollY));
+
+    return () => { // return a cleanup function to unregister our function since its gonna run multiple times
+      window.removeEventListener("scroll", (e) => setY(window.scrollY));
+    };
+  }, [y]);
   return (
     <>
       <Header />
       <Main />
       <Footer />
-      <ScrollToTop />
+      {y >= 560 && <ScrollToTop />}
     </>
   );
 }
