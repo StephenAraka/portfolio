ef674787eb807657eb3311f21afb35ecc37bc0ce
StephenAraka <stevenaraka@gmail.com>
2025-02-05 10:31:59 +0300

    Updates

---
+++ precache-manifest.a94465e58ffea944687b3306fbd6357b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85a9213962bedfaca975ea886ac38a07",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2cf3908d945a2c5466d7",
+    "url": "/portfolio/static/css/2.c16044e5.chunk.css"
+  },
+  {
+    "revision": "2c3a9b7fb252dcab4cb5",
+    "url": "/portfolio/static/css/main.1421c083.chunk.css"
+  },
+  {
+    "revision": "2cf3908d945a2c5466d7",
+    "url": "/portfolio/static/js/2.24efdb92.chunk.js"
+  },
+  {
+    "revision": "2c3a9b7fb252dcab4cb5",
+    "url": "/portfolio/static/js/main.cd2e686c.chunk.js"
+  },
+  {
+    "revision": "e73ac9e279836ecdfedf",
+    "url": "/portfolio/static/js/runtime-main.aa1db941.js"
+  },
+  {
+    "revision": "be144b818c013c22a60fe607b7574f0c",
+    "url": "/portfolio/static/media/about.be144b81.jpg"
+  },
+  {
+    "revision": "59a1d9d9814b112c2224da5137b0b74a",
+    "url": "/portfolio/static/media/cc.59a1d9d9.png"
+  },
+  {
+    "revision": "a01a8ae772b1f1af4ea5e080b3ab30f0",
+    "url": "/portfolio/static/media/coast.a01a8ae7.png"
+  },
+  {
+    "revision": "ed9f1e450cc597824e106732380dab6d",
+    "url": "/portfolio/static/media/contact.ed9f1e45.png"
+  },
+  {
+    "revision": "e36cff0fb45c162eb24147677ca64f7a",
+    "url": "/portfolio/static/media/covid.e36cff0f.png"
+  },
+  {
+    "revision": "ba9bd3340331ddc1c3c9a5ac03123113",
+    "url": "/portfolio/static/media/pets.ba9bd334.png"
+  }
+]);
